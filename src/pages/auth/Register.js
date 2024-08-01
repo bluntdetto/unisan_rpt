b325d6a0e062060f931063fc7727daf1b4eb5f3c
blundetto <efraymjohnvaller@gmail.com>
2024-08-02 04:23:09 +0800

    1st commit

---
+++ src/pages/auth/Register.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Register = () => {
+  return (
+    <form>
+      <h3 className="text-center mb-3">Create An Account</h3>
+    </form>
+  );
+};
+
+export default Register;
